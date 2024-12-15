eb0810052fd907f07316379cbf90d2df7cb99812
MykytaKononov <nikitamaxkononov@gmail.com>
2024-12-15 18:46:32 +0100

    done

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+class StringBuilder {
+  constructor(params) {
+    this.value = params;
+  }
+  getValue() {
+    return this.value;
+  }
+  padEnd(str) {
+    return (this.value = this.value + str);
+  }
+  padStart(str) {
+    return (this.value = str + this.value);
+  }
+  padBoth(str) {
+    return (this.value = str + this.value + str);
+  }
+}
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
